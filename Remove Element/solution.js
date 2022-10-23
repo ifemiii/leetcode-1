7ab1322cf3be3f0bf2bc3e0ef3edac209e90d2eb
Ifeoluwa Ogungbemi <79316168+ifemiii@users.noreply.github.com>
2022-10-23 21:34:37 +0100

    Create solution.js

---
+++ Remove Element/solution.js
@@ -0,0 +1,10 @@
+	function removeElement (nums, val) {		
+    if(!nums) return 0;		
+    for(let i = 0; i < nums.length; i++){		
+      if(nums[i]===val){		
+      nums.splice(i,1);		
+      i--		
+     }		
+   }			
+	return nums.lenght;		
+	};		
